8babb971d16f60460a98139e8e50f801ee432f4d
Ken Robertson <ken@invalidlogic.com>
2013-12-30 13:08:01 -0800

    Remove version constaints to prevent clashes with custom cookbooks

--- metadata.rb
+++ metadata.rb
@@ -12,8 +12,8 @@ recipe "database::ebs_volume", "Sets up an EBS volume in EC2 for the database"
 recipe "database::master", "Creates application specific user and database"
 recipe "database::snapshot", "Locks tables and freezes XFS filesystem for replication, assumes EC2 + EBS"
 
-depends "mysql", ">= 1.3.0"
-depends "postgresql", ">= 1.0.0"
+depends "mysql"
+depends "postgresql"
 depends "aws"
 depends "xfs"
 
